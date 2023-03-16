e0e206d2fd869ea90f672032241a76e4b287a322
q0w <43147888+q0w@users.noreply.github.com>
2023-03-17 02:19:43 +0300

    Apply ruff

--- pdm_update_interactive.py
+++ pdm_update_interactive.py
@@ -12,7 +12,7 @@ from pdm.cli.hooks import HookManager
 from pdm.cli.utils import fetch_hashes
 from pdm.resolver.core import resolve
 from pdm.signals import pre_lock
-from questionary import checkbox, Choice
+from questionary import Choice, checkbox
 
 if TYPE_CHECKING:
     from pdm.core import Core
@@ -29,29 +29,29 @@ class InteractiveHookManager(HookManager):
 def pre_lock_signal(
     project: Project,
     requirements: list[Requirement],
-    dry_run: bool,
+    dry_run: bool,  # noqa: FBT001
     hooks: HookManager,
 ) -> None:
     if not isinstance(hooks, InteractiveHookManager):
         return
     lock_frame, update_frame = None, None
     for f in inspect.stack():
-        if f.function == "do_lock":
+        if f.function == 'do_lock':
             lock_frame = f
-        if f.function == "do_update":
+        if f.function == 'do_update':
             update_frame = f
         if lock_frame and update_frame:
             break
     else:
         return
 
-    spinner = lock_frame.frame.f_locals["spin"]
-    provider = lock_frame.frame.f_locals["provider"]
+    spinner = lock_frame.frame.f_locals['spin']
+    provider = lock_frame.frame.f_locals['provider']
     with spinner:
         reporter = project.get_reporter(
             requirements,
             provider.tracked_names,
-            lock_frame.frame.f_locals["spin"],
+            lock_frame.frame.f_locals['spin'],
         )
         resolver: Resolver[
             Requirement,
@@ -62,23 +62,23 @@ def pre_lock_signal(
             resolver,
             requirements,
             project.environment.python_requires,
-            lock_frame.frame.f_locals["resolve_max_rounds"],
+            lock_frame.frame.f_locals['resolve_max_rounds'],
         )
         fetch_hashes(provider.repository, mapping)
     current_candidates = project.locked_repository.all_candidates
     project_dependencies = set(
         chain.from_iterable(
             project.get_dependencies(group)
-            for group in update_frame.frame.f_locals["groups"]
+            for group in update_frame.frame.f_locals['groups']
         ),
     )
     deps_to_update = [
-        Choice(f"{name} {c.version} > {mapping[name].version}", name)
+        Choice(f'{name} {c.version} > {mapping[name].version}', name)
         for name, c in current_candidates.items()
         if c.version != mapping[name].version and name in project_dependencies
     ]
     response = checkbox(
-        "Choose dependencies to update...",
+        'Choose dependencies to update...',
         choices=deps_to_update,
     ).ask()
     prompt_deps = set(response) if response else set()
@@ -107,17 +107,17 @@ def pre_lock_signal(
 
     lock_frame.frame.f_locals.update(
         {
-            "mapping": mapping,
-            "dependencies": collected_dependencies,
-            "resolver": resolver,
-            "reporter": reporter,
-            "provider": provider,
+            'mapping': mapping,
+            'dependencies': collected_dependencies,
+            'resolver': resolver,
+            'reporter': reporter,
+            'provider': provider,
         },
     )
     lock_frame.frame.f_globals.update(
         {
-            "resolve": lambda *args: (mapping, collected_dependencies),
-            "fetch_hashes": lambda *args: None,
+            'resolve': lambda *args: (mapping, collected_dependencies),
+            'fetch_hashes': lambda *args: None,
         },
     )
     ctypes.pythonapi.PyFrame_LocalsToFast(
@@ -131,10 +131,10 @@ class Command(BaseCommand):
         project_groups = (
             list(project.iter_groups())
             if options.default
-            else [p for p in project.iter_groups() if p != "default"]
+            else [p for p in project.iter_groups() if p != 'default']
         )
         selected_groups = checkbox(
-            "Choose dependency groups...",
+            'Choose dependency groups...',
             choices=project_groups,
         ).ask()
         if not selected_groups:
@@ -145,8 +145,8 @@ class Command(BaseCommand):
             dev=options.dev,
             groups=selected_groups,
             default=options.default,
-            save=options.save_strategy or project.config["strategy.save"],
-            strategy=options.update_strategy or project.config["strategy.update"],
+            save=options.save_strategy or project.config['strategy.save'],
+            strategy=options.update_strategy or project.config['strategy.update'],
             unconstrained=options.unconstrained,
             top=options.top,
             dry_run=options.dry_run,
@@ -161,4 +161,4 @@ class Command(BaseCommand):
 
 def update_interactive(core: Core) -> None:
     pre_lock.connect(pre_lock_signal)
-    core.register_command(Command, "update-interactive")
+    core.register_command(Command, 'update-interactive')
